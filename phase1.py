38f89de8c517c949ac2c2feefb3c2f4b16f188bf
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-13 06:14:43 +0545

    warning

--- phase1.py
+++ phase1.py
@@ -9,3 +9,7 @@ import os
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+
+
+# Disable warnings and info logs
+warnings.filterwarnings("ignore")
