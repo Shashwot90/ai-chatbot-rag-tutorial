3c28eabf3d2a2fa11529eb72d50ed662244de0d6
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-28 06:38:43 +0545

    hugging

--- phase3.py
+++ phase3.py
@@ -9,3 +9,4 @@ import os
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
