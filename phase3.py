510342fd4322b36d32dab21dc571677d9fb1b834
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-28 06:38:31 +0545

    chat

--- phase3.py
+++ phase3.py
@@ -8,3 +8,4 @@ import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
