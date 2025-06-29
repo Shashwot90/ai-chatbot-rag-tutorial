08dd15e6fdd31236ac371581c7915948debb8675
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-29 06:50:27 +0545

    recursive

--- phase3.py
+++ phase3.py
@@ -10,3 +10,4 @@ from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
