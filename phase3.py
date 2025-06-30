4b74bd11cd77e5035dc48dfc1f267cab0b24d04c
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-30 06:20:48 +0545

    retreive

--- phase3.py
+++ phase3.py
@@ -13,3 +13,4 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
